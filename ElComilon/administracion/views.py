199f99c2af53a1fed0eaa749caf02abba3c14fb9
Nico <nicoztk619@gmail.com>
2021-10-08 13:13:40 -0300

    2.3.5

--- ElComilon/administracion/views.py
+++ ElComilon/administracion/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def administracion(request):
-    return render(request,'admin.html')
+    return render(request,'registro-proveedor.html')
     
