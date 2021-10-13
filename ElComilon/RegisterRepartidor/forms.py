139506922499004970a4805a02566253524dd831
Nico <nicoztk619@gmail.com>
2021-10-12 22:41:25 -0300

    2.3.12

--- ElComilon/RegisterRepartidor/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from core.models import *
-
-class Repartidorform(forms.ModelForm):
-    class Meta:
-        model = Repartidor
-        fields = ('__all__')
--- ElComilon/administracion/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def administracion(request):
-    return render(request,'admin.html')
-    
